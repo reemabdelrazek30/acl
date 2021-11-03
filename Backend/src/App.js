84ab0d8faa861c9c312767fa5d374bbd453fe80e
Esraa Ahmed <esraa.elashmawy1234@gmail.com>
2021-11-03 22:00:13 +0200

    I did the view flight

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import CreateFlight from "./Components/CreateFlight";
+import Flight from "./Components/Flight";
+
 // if you got here that means I am crying 
 // Hallo
 function App() {
   return (
     <div className="App">
-      <Router>
+     {/*  <Flight/>*/}
+
+  {/*    <Router>
         <Switch>
              <Route exactpath = "/createFlight" component={CreateFlight}/>
           </Switch>
-        </Router>
+  </Router> */}
     </div>
   );
 }
