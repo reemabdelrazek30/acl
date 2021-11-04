b1809ad222d00f95ffb4f956e999949b72fa1fa5
Nouran <nourakhaled329@gmail.com>
2021-11-04 20:58:31 +0200

    udate method

--- Server/index.js
+++ Server/index.js
@@ -26,13 +26,66 @@ app.get("/", (req, res) => {
 
 app.post('/addFlight', async (req,res) =>
 {
-    console.log("here");
-    console.log(req.body);  //Sha8al
-    const new_flight = new Flight(req.body);
-    console.log('passed'); // it never comes here
+    // console.log("here");
+    
+    // console.log(req.body);  //Sha8al
+    // const new_flight = new Flight(req.body);
+    // console.log('passed'); // it never comes here
+    const Flight_Number=(req.body.Flight_Number)
+  const Departure_Date= Date.parse(req.body.Departure_Date)
+  const Departure_Time=req.body.Departure_Time
+  const Arrival_Date= Date.parse(req.body.Arrival_Date)
+  const Arrival_Time=req.body.Arrival_Time
+  const Departure_Airport=req.body.Departure_Airport
+  const Arrival_Airport=req.body.Arrival_Airport
+  const Number_of_Economy_seats= Number(req.body.Number_of_Economy_seats)
+  
+  const Number_of_Business_seats= Number(req.body.Number_of_Business_seats)
+
+  const new_flight= new Flight({
+    Flight_Number,Departure_Date,Departure_Time,Arrival_Date,Arrival_Time,Departure_Airport,Arrival_Airport,Number_of_Economy_seats,Number_of_Business_seats
+  })
     await new_flight.save().then(()=> res.json('flight is added')).catch(err => res.status(400).json('Error: '+err))
 });
+app.put('/updateFlight', async (req,res) =>
+{
+    // console.log("here");
+    
+    // console.log(req.body);  //Sha8al
+    // const new_flight = new Flight(req.body);
+    // console.log('passed'); // it never comes here
+    const id=(req.body.id)
+    const Flight_Number=(req.body.Flight_Number)
+  const Departure_Date= Date.parse(req.body.Departure_Date)
+  const Departure_Time=req.body.Departure_Time
+  const Arrival_Date= Date.parse(req.body.Arrival_Date)
+  const Arrival_Time=req.body.Arrival_Time
+  const Departure_Airport=req.body.Departure_Airport
+  const Arrival_Airport=req.body.Arrival_Airport
+  const Number_of_Economy_seats= Number(req.body.Number_of_Economy_seats)
+  
+  const Number_of_Business_seats= Number(req.body.Number_of_Business_seats)
+try{
+  await Flight.findById(id,(err,newf)=>{
+    newf.Flight_Number=Flight_Number
+    newf.Departure_Date=Departure_Date
+    newf.Departure_Time=Departure_Time
+    newf.Arrival_Date=Arrival_Date
+    newf.Arrival_Time=Arrival_Time
+    newf.Departure_Airport=Departure_Airport
+    newf.Arrival_Airport=Arrival_Airport
+    newf.Number_of_Economy_seats=Number_of_Economy_seats
+    newf.Number_of_Business_seats=Number_of_Business_seats
+    newf.save()
+    res.send("updated")
 
+  })
+}catch(err){
+  console.log(err)
+}
+  
+   
+});
 
 app.get("/searchByflightNumber/:flight_Number", (req, res) => {
     Flight.find({"Flight_Number":req.params.flight_Number})
